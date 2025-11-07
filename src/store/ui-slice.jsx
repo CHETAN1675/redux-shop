c0a8a0f856c47b5b8b4d8028f8c2ebdfa9da8c68
CHETAN1675 <mahajanchetan001@gmail.com>
2025-11-07 18:06:06 +0530

    Fixing the cart problem

--- src/store/ui-slice.jsx
+++ src/store/ui-slice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name:"ui",
-    initialState:{cartIsVisible:false},
+    initialState:{cartIsVisible:false,notification:null},
     reducers:{
         toggle(state){
             state.cartIsVisible=!state.cartIsVisible;
@@ -13,11 +13,11 @@ const uiSlice = createSlice({
                 status: action.payload.status,
               title: action.payload.title,
               message: action.payload.message,
-            }
-        
+            };
         }
     },
 });
+
 export const uiActions = uiSlice.actions;
 
 export default uiSlice.reducer;
