725d56b18112e0e95988f4f0b188cc4386fa7597
CHETAN1675 <mahajanchetan001@gmail.com>
2025-11-07 19:20:05 +0530

    Exploring createAsyncThunk in Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
 import Notification from "./components/UI/Notification";
-import { fetchCartData,sendCartData} from "./store/cart-actions";
+import { fetchCartData} from "./store/cart-actions";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true ;
 
--- src/store/cart-slice.jsx
+++ src/store/cart-slice.jsx
@@ -1,23 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {uiActions} from "./ui-slice";
 
+export const sendCartData = createAsyncThunk(
+  "cart/sendCartData",
+   async (cart, { dispatch, rejectWithValue }) => {
+    // Dispatch pending notification before sending
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+   );
+
+    try {
+      const response = await fetch(
+        "https://redux-shop-data-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+
+      // Retur's a success message
+      return "Sent cart data successfully!";
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 const cartSlice = createSlice({
     name : 'cart',
     initialState:{
         items:[],
         totalQuantity:0,
+        status:null,
     },
     reducers:{
       replaceCart(state,action){
         state.totalQuantity=action.payload.totalQuantity;
-        state.items =  action.payload.items;
+        state.items =  action.payload.items||[];
       },
         addItemCart(state,action){
             const newItem = action.payload;
             const existingItem = state.items.find(item=>item.id===newItem.id)
             state.totalQuantity++;
 
-             if (!existingItem) {
+        if (!existingItem) {
         state.items.push({
           id: newItem.id,
           title: newItem.title,
@@ -43,6 +80,18 @@ const cartSlice = createSlice({
         existingItem.totalPrice -= existingItem.price;
       }
     },
+    },
+     extraReducers: (builder) => {
+    builder
+      .addCase(sendCartData.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(sendCartData.fulfilled, (state, action) => {
+        state.status = "succeeded";
+      })
+      .addCase(sendCartData.rejected, (state, action) => {
+        state.status = "failed";
+      });
     },
 });
 
